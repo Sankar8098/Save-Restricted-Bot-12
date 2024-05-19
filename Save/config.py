1d5a13bd5917f9c6bf96a4405c0718472e285165
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-05-19 16:24:35 +0530

    Update config.py

--- Save/config.py
+++ Save/config.py
@@ -1,1 +1,10 @@
+import os
 
+class Config(object):
+     
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", ))
+    API_HASH = os.environ.get("API_HASH", "")
+    STRING = os.environ.get("STRING", "")
+    
+    
