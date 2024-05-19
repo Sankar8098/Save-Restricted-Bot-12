17c9a8bbe2dab980f60f1c495c87039778b31258
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-05-19 22:42:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,20 @@ from Save.config import Config
 
 
 
-app = Client("mybot", bot_token=Config.BOT_TOKEN, api_id=Config.API_ID, api_hash=Config.API_HASH, plugins=dict(root="Save"))
+app = Client(
+    "mybot", 
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    plugins=dict(root="Save"))
 
 
 if Config.STRING is not None:
-    acc = Client("myacc", api_id=Config.API_ID, api_hash=Config.API_HASH, session_string=Config.STRING)
+    acc = Client(
+        "myacc",
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        session_string=Config.STRING)
     acc.start()
 else:
     acc = None
