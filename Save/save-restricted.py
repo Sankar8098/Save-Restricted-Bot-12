ab44c1b7778f0be7ec2206438b316889f0fe24ec
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-05-20 22:27:38 +0530

    Update save-restricted.py

--- Save/save-restricted.py
+++ Save/save-restricted.py
@@ -1,1 +1,7 @@
+# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | LISA-KOREA/Save-Restricted-Bot
+
+# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/Save-Restricted-Bot
+
+
+
 " soon "
